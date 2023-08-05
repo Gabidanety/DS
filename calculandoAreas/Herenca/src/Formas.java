6450fdb6c8ecf7816fba502f85a41f9c4dd276d9
Gabizinha <gabinete878@gmail.com>
2023-08-05 20:06:31 -0300

    Add files via upload

---
+++ calculandoAreas/Herenca/src/Formas.java
@@ -0,0 +1,21 @@
+
+public class Formas {
+		public int base;
+		public int h;
+		public int area;
+		
+
+
+		public void setCalculandoArea(int base, int h) {
+			this.base = base;
+			this.h = h;
+
+		}
+
+		
+
+		
+		
+		
+		
+}
